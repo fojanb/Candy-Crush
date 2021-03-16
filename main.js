0f4a3026e62127498924bd7a6d80c464e7d3bf98
Fojan Babaali <fozhanmail@gmail.com>
2021-03-15 18:24:25 -0700

    Update main.js

--- main.js
+++ main.js
@@ -95,16 +95,41 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
   //---------------------Drag Candies end------------------*
+  // Drop down candies once some have been cleared
+  function moveDown() {
+    for (let i = 0; i < 55; i++) {
+      if (boxes[i + width].style.backgroundColor === "") {
+        boxes[i + width].style.backgroundColor = boxes[i].style.backgroundColor;
+        boxes[i].style.backgroundColor = "";
+      }
+    }
+  }
+  //--------------------Cheking for matches-----------------*
+  // <<rows of 3 or 4 or 5 candies / colums of 3 or 4 or 5 candies>>
 
-  // Cheking for matches : rows of 3 or 4 or 5 candies / colums of 3 or 4 or 5 candies
-  // Check for row of 3 candies :
+  // 1- Check for row of 3 candies :
   function checkRowForThree() {
     for (let i = 0; i < boxes.length - 3; i++) {
       let rowOfThree = [i, i + 1, i + 2];
       let decidedColor = boxes[i].style.backgroundColor;
       const isBlank = boxes[i].style.backgroundColor === "";
-      let notValidStartBoxIndexes=[6,7,14,15,22,23,30,31,38,39,46,47,54,55];
-      if(notValidStartBoxIndexes.includes(i)) continue
+      let notValidStartBoxIndexes = [
+        6,
+        7,
+        14,
+        15,
+        22,
+        23,
+        30,
+        31,
+        38,
+        39,
+        46,
+        47,
+        54,
+        55,
+      ];
+      if (notValidStartBoxIndexes.includes(i)) continue;
       if (
         rowOfThree.every(
           (item) =>
@@ -120,7 +145,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   checkRowForThree();
 
-  // Check for column of 3 candies :
+  // 2- Check for column of 3 candies :
   function checkColumnForThree() {
     for (let i = 0; i < 47; i++) {
       let columnOfThree = [i, i + 8, i + 16];
@@ -142,59 +167,80 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   checkColumnForThree();
 
-// Check for row of 4 candies :
-function checkRowForFour() {
-  for (let i = 0; i < boxes.length - 4; i++) {
-    let rowOfFour = [i, i + 1, i + 2, i + 3];
-    let decidedColor = boxes[i].style.backgroundColor;
-    const isBlank = boxes[i].style.backgroundColor === "";
-    let notValidStartBoxIndexes=[5,6,7,13,14,15,21,22,23,29,30,31,37,38,39,45,46,47,53,54,55];
-    if(notValidStartBoxIndexes.includes(i)) continue
-    if (
-      rowOfFour.every(
-        (item) =>
-          boxes[item].style.backgroundColor === decidedColor && !isBlank
-      )
-    ) {
-      score += 4;
-      rowOfFour.forEach((item) => {
-        boxes[item].style.backgroundColor = "";
-      });
-    } //End of if
-  } //End of for
-}
-checkRowForFour();
+  // 3- Check for row of 4 candies :
+  function checkRowForFour() {
+    for (let i = 0; i < boxes.length - 4; i++) {
+      let rowOfFour = [i, i + 1, i + 2, i + 3];
+      let decidedColor = boxes[i].style.backgroundColor;
+      const isBlank = boxes[i].style.backgroundColor === "";
+      let notValidStartBoxIndexes = [
+        5,
+        6,
+        7,
+        13,
+        14,
+        15,
+        21,
+        22,
+        23,
+        29,
+        30,
+        31,
+        37,
+        38,
+        39,
+        45,
+        46,
+        47,
+        53,
+        54,
+        55,
+      ];
+      if (notValidStartBoxIndexes.includes(i)) continue;
+      if (
+        rowOfFour.every(
+          (item) =>
+            boxes[item].style.backgroundColor === decidedColor && !isBlank
+        )
+      ) {
+        score += 4;
+        rowOfFour.forEach((item) => {
+          boxes[item].style.backgroundColor = "";
+        });
+      } //End of if
+    } //End of for
+  }
+  checkRowForFour();
 
-// Check for column of 4 candies :
-function checkColumnForFour() {
-  for (let i = 0; i < 47; i++) {
-    let columnOfFour = [i, i + 8, i + 16, i + 24];
-    let decidedColor = boxes[i].style.backgroundColor;
-    const isBlank = boxes[i].style.backgroundColor === "";
+  // 4- Check for column of 4 candies :
+  function checkColumnForFour() {
+    for (let i = 0; i < 47; i++) {
+      let columnOfFour = [i, i + 8, i + 16, i + 24];
+      let decidedColor = boxes[i].style.backgroundColor;
+      const isBlank = boxes[i].style.backgroundColor === "";
 
-    if (
-      columnOfFour.every(
-        (item) =>
-          boxes[item].style.backgroundColor === decidedColor && !isBlank
-      )
-    ) {
-      score += 4;
-      columnOfFour.forEach((item) => {
-        boxes[item].style.backgroundColor = "";
-      });
-    } //End of if
-  } //End of for
-}
-checkColumnForFour();
-
-
+      if (
+        columnOfFour.every(
+          (item) =>
+            boxes[item].style.backgroundColor === decidedColor && !isBlank
+        )
+      ) {
+        score += 4;
+        columnOfFour.forEach((item) => {
+          boxes[item].style.backgroundColor = "";
+        });
+      } //End of if
+    } //End of for
+  }
+  checkColumnForFour();
 
   window.setInterval(function () {
+    moveDown();
     checkRowForFour();
     checkColumnForFour();
     checkRowForThree();
     checkColumnForThree();
-    
+  }, 100); //Invoke every 100 milli seconds
 
-  }, 100); //Invoke every 100 mili sec
+  
 });
